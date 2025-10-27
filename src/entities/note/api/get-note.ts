c30e242d6d714f1ccdb8e3ba8acb8957bfc75fa8
George Susyev <no1621name@gmail.com>
2025-10-27 09:20:36 +0300

    get-note action fix

--- src/entities/note/api/get-note.ts
+++ src/entities/note/api/get-note.ts
@@ -1,7 +1,7 @@
 import type { PrimaryKeyType, DataTransfer } from '@/shared/types/api';
-import type { Note } from '../model/interface';
+import type { NoteData } from '../model/types';
 import { storeConfig } from './store-config';
 
-export const getNote = (dataTransfer: DataTransfer, id: PrimaryKeyType) => {
-  return dataTransfer.getById<Note>(storeConfig.name, id);
+export const getNote = async (dataTransfer: DataTransfer, id: PrimaryKeyType) => {
+  return dataTransfer.getById<NoteData>(storeConfig.name, id);
 };
