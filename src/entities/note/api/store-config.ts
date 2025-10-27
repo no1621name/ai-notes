d041d40d4444541a1e93f7239f2fe2bae0987770
George Susyev <no1621name@gmail.com>
2025-10-27 09:20:02 +0300

    added notes-tags relation store

--- src/entities/note/api/store-config.ts
+++ src/entities/note/api/store-config.ts
@@ -1,13 +1,34 @@
 import { type DataStore, SchemaFieldType } from '@/shared/types/api';
-import type { Note } from '../model/interface';
+import type { NoteData } from '../model/types';
 
-const noteSchema: Record<Exclude<keyof Note, 'id'>, SchemaFieldType> = {
+const noteSchema: Record<Exclude<keyof NoteData, 'id' | 'tags'>, SchemaFieldType> = {
   created_at: SchemaFieldType.NOT_UNIQUE,
-  name: SchemaFieldType.UNIQUE,
+  title: SchemaFieldType.NOT_UNIQUE,
+  text: SchemaFieldType.NOT_UNIQUE,
+  reminder_date: SchemaFieldType.NOT_UNIQUE,
 };
 
 export const storeConfig: DataStore = {
   name: 'notes',
   primaryKey: 'id',
   schema: noteSchema,
+};
+
+export const relationConfig: DataStore = {
+  name: 'note_tags',
+  primaryKey: 'id',
+  schema: {
+    note_id: SchemaFieldType.NOT_UNIQUE,
+    tag_id: SchemaFieldType.NOT_UNIQUE,
+  },
+  indexes: {
+    noteId: {
+      name: 'note_id_index',
+      keyPath: 'note_id',
+    },
+    tagId: {
+      name: 'tag_id_index',
+      keyPath: 'tag_id',
+    },
+  },
 };
