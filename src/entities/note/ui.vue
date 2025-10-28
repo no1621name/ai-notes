14f757dcdebeef64a27e5990d1401d45938acf8b
George Susyev <no1621name@gmail.com>
2025-10-28 22:19:04 +0300

    refactor(note/ui): simplify prop access and display full note data

--- src/entities/note/ui.vue
+++ src/entities/note/ui.vue
@@ -1,12 +1,9 @@
 <script lang="ts" setup>
-import { toRefs } from 'vue';
 import { useGetNote } from './queries/use-get-note';
 const props = defineProps<{ id: string }>();
-const pr = toRefs(props);
-const { data, isLoading } = useGetNote(pr.id);
+const { data, isLoading } = useGetNote(props.id);
 </script>
 
 <template>
-  <div>{{ isLoading ? 'loading' : data?.id }}</div>
-  <!-- <Button @click="addNote">create</Button> -->
+  <div>{{ isLoading ? 'loading' : JSON.stringify(data) }}</div>
 </template>
