3e9ddb6b306931804a30030b12f47bff4c61159d
George Susyev <no1621name@gmail.com>
2025-11-04 01:07:42 +0300

    tag: new reusable types for notes

--- src/entities/tag/@x/note.ts
+++ src/entities/tag/@x/note.ts
@@ -1,2 +1,7 @@
+import type { TagBody } from '../api/contracts';
+
+export type WithTags<T> = T & { tags: TagBody[] };
+
 export type { Tag } from '../model/types';
 export { storeConfig as tagsStoreConfig } from '../api/store-config';
+export type { TagBody };
