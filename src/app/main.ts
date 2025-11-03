36d00a07eeaf9f44d4560cabc2297bc5ef6d23a7
George Susyev <no1621name@gmail.com>
2025-11-04 01:03:52 +0300

    added "click-outside" vue directive

--- src/app/main.ts
+++ src/app/main.ts
@@ -9,6 +9,7 @@ import { VueQueryPlugin } from '@tanstack/vue-query';
 import App from './app.vue';
 import router from './router';
 import { createDBDataTransfer, dbDataTransferKey, createLsDataTransfer, lsDataTransferKey } from './providers/data-transfer';
+import { clickOutsideDirective } from '@/shared/lib/click-outside-directive';
 import { ErrorNotifier } from '@/shared/api/errors/error-notifier';
 import './styles/main.css';
 
@@ -16,6 +17,8 @@ const configureApp = (app: IApp) => {
   app.use(VueQueryPlugin);
   app.use(createPinia());
   app.use(router);
+
+  app.directive('click-outside', clickOutsideDirective);
 
   const errorNotifier = new ErrorNotifier();
 
