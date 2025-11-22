5983782bec9551d6979890007d5d9b00885ef4b0
George Susyev <no1621name@gmail.com>
2025-11-22 22:56:57 +0300

    fix: removed timeout for note api call

--- src/entities/note/api/tags/get-note-with-tags.ts
+++ src/entities/note/api/tags/get-note-with-tags.ts
@@ -5,12 +5,7 @@ import { ManyToManyManager } from '@/shared/api/db/relations/many-to-many';
 import type { NoteBody } from '../contracts';
 import { noteToTagRelationConfig } from '../store-config';
 
-async function sleep(ms: number): Promise<void> {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
 export const getNoteWithTags = async (dataTransfer: DBDataTransfer, noteId: PrimaryKeyType): Promise<WithTags<NoteBody>> => {
   const relationManager = new ManyToManyManager(noteToTagRelationConfig, dataTransfer);
-  await sleep(3000);
   return relationManager.get<NoteBody, TagBody, 'tags'>(noteId);
 };
