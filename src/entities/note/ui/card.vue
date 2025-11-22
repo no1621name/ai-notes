49452d1a6ca500240fba6d61e16012a78363c8f3
George Susyev <no1621name@gmail.com>
2025-11-22 22:46:11 +0300

    note: added base ui components

---
+++ src/entities/note/ui/card.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import type { NoteShort } from '../model/types';
+import { formatDate, DateFormat } from '@/shared/lib/date';
+
+interface DateItem {
+  label: string;
+  value: string | null;
+}
+
+const props = defineProps<{
+  note: NoteShort;
+}>();
+
+const routeParams = computed(() => ({
+  name: 'note-details',
+  params: { id: props.note.id },
+}));
+
+const dateItems = computed<DateItem[]>(() => {
+  const { created_at, updated_at, reminder_date } = props.note;
+  return [
+    { label: 'Created', value: formatDate(created_at) },
+    { label: 'Updated', value: formatDate(updated_at, DateFormat.WITH_TIME) },
+    { label: 'Reminder', value: formatDate(reminder_date, DateFormat.WITH_TIME) },
+  ];
+});
+</script>
+
+<template>
+  <RouterLink :to="routeParams" class="card bg-base-200 shadow-xl h-full">
+    <div class="card-body">
+      <h2 class="card-title break-all">{{ note.title }}</h2>
+
+      <div class="text-ellipsis">
+        <p class="text-base-content/70 line-clamp-5">{{ note.description }}</p>
+      </div>
+
+      <div class="card-actions mt-auto">
+        <div class="flex-grow">
+          <div v-if="note.tags && note.tags.length" class="flex flex-wrap gap-2">
+            <slot name="tags" :tags="note.tags" />
+          </div>
+        </div>
+      </div>
+
+      <div class="text-xs text-base-content/50 mt-2 flex flex-col gap-0.5">
+        <template v-for="item in dateItems" :key="item.label">
+          <span v-if="item.value">
+            {{ item.label }}: {{ item.value }}
+          </span>
+        </template>
+      </div>
+    </div>
+  </RouterLink>
+</template>
---
+++ src/entities/note/ui/title.vue
@@ -0,0 +1,102 @@
+<script lang="ts" setup>
+import { computed } from 'vue';
+import { z } from 'zod';
+import { useField } from 'vee-validate';
+import { toTypedSchema } from '@vee-validate/zod';
+
+withDefaults(defineProps<{
+  modelValue: string | null;
+  skeleton?: boolean;
+}>(), {
+  skeleton: false,
+});
+
+const MAX_LEN = 100;
+
+const { value, errorMessage } = useField(
+  'title',
+  toTypedSchema(z.string().trim().min(1, 'Required')),
+  { syncVModel: true },
+);
+
+const currentLength = computed(() => value.value?.length ?? 0);
+
+const preventTyping = (event: KeyboardEvent) => {
+  if (event.ctrlKey || event.metaKey || event.altKey) return;
+
+  if (
+    event.key === 'Backspace'
+    || event.key === 'Delete'
+    || event.key.startsWith('Arrow')
+  ) {
+    return;
+  }
+
+  if (event.key === ' ' || event.key === 'Enter') {
+    const input = event.target as HTMLInputElement | HTMLTextAreaElement;
+    const currentText = input.value;
+    const selectionStart = input.selectionStart ?? 0;
+
+    if (selectionStart === 0 && currentText.length === 0) {
+      event.preventDefault();
+      return;
+    }
+
+    if (selectionStart > 0 && (currentText[selectionStart - 1] === ' ' || currentText[selectionStart - 1] === '\n')) {
+      event.preventDefault();
+      return;
+    }
+  }
+
+  if (currentLength.value >= MAX_LEN) {
+    event.preventDefault();
+    return;
+  }
+};
+
+const preventPasting = (event: ClipboardEvent) => {
+  event.preventDefault();
+
+  const input = event.target as HTMLInputElement | HTMLTextAreaElement;
+  const pasteText = event.clipboardData?.getData('text/plain') ?? '';
+  if (!pasteText) return;
+
+  const start = input.selectionStart ?? 0;
+  const end = input.selectionEnd ?? 0;
+  const currentText = input.value;
+
+  const availableSpace = MAX_LEN - (currentText.length - (end - start));
+
+  if (availableSpace <= 0) return;
+
+  const insertText = pasteText.slice(0, availableSpace);
+
+  const newValue = currentText.slice(0, start) + insertText + currentText.slice(end);
+
+  value.value = newValue;
+
+  const newCursorPos = start + insertText.length;
+  requestAnimationFrame(() => {
+    input.setSelectionRange(newCursorPos, newCursorPos);
+  });
+};
+
+</script>
+
+<template>
+  <fieldset class="relative">
+    <textarea
+      @keydown="preventTyping"
+      @paste="preventPasting"
+      v-model.trim="value"
+      placeholder="Note title..."
+      :disabled="skeleton"
+      type="text"
+      class="textarea textarea-xl textarea-ghost field-sizing-content max-h-32 min-h-0 overflow-y-auto w-full max-w-full resize-none focus:outline-0 break-all"
+    />
+    <div class="flex items-center flex-wrap mt-1">
+      <p v-if="!!errorMessage" class="mr-auto text-error text-sm">{{errorMessage}}</p>
+      <span class="select-none text-base-content/50 text-sm ml-auto">{{currentLength}}/{{MAX_LEN}}</span>
+    </div>
+  </fieldset>
+</template>
