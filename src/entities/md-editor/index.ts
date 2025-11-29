3c4c90e83094f7cdeede689e5582b48fa7ace8b1
George Susyev <no1621name@gmail.com>
2025-11-30 00:04:42 +0300

    md-editor: updated reexport

--- src/entities/md-editor/index.ts
+++ src/entities/md-editor/index.ts
@@ -1,3 +1,6 @@
 export { default as MainBubbleMenu } from './ui/main-bubble-menu.vue';
 export { default as Editor } from './ui/editor.vue';
+export { default as FormattingActions } from './ui/formatting/formatting-actions.vue';
+
 export { editorInjectionKey } from './model/keys';
+export { resetEditorContent } from './lib/reset-editor-content';
