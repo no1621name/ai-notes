a86ffba97aa82ce7524c973cfddd3a7140c66d3b
George Susyev <no1621name@gmail.com>
2025-11-30 18:40:45 +0300

    added types for env vars

--- env.d.ts
+++ env.d.ts
@@ -1,3 +1,13 @@
 /// <reference types="vite/client" />
 
 declare module '@kalimahapps/vue-icons/vite';
+
+interface ImportMetaEnv {
+  readonly VITE_VAPID_PUBLIC_JWK: string;
+  readonly VITE_SUPABASE_URL: string;
+  readonly VITE_SUPABASE_ANON_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
