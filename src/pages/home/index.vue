2ec6745611119a69aadb6108fb26cf75ee22ea08
George Susyev <no1621name@gmail.com>
2025-12-07 04:50:50 +0300

    pages/home: using new feature component NotesList

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -5,33 +5,15 @@ import type { Editor as TipTapEditor } from '@tiptap/vue-3';
 
 import { editorInjectionKey } from '@/entities/md-editor';
 
-import { TagBadge } from '@/entities/tag';
-import { CreateNoteLink, NoteCard, useGetNotes } from '@/entities/note';
+import NotesList from '@/features/note/notes-list.vue';
 
 const editor = shallowRef<{ editor: TipTapEditor } | null>(null);
 
 provide(editorInjectionKey, editor);
-
-const { data } = useGetNotes();
 </script>
 
 <template>
   <div>
-    <div class="grid grid-cols-3 auto-rows-fr gap-3 m-3">
-      <CreateNoteLink/>
-      <NoteCard
-        v-for="note in data"
-        :key="note.id"
-        :note="note"
-      >
-        <template #tags="{ tags }">
-          <TagBadge
-            v-for="tag in tags"
-            :key="tag.id"
-            :tag="tag"
-          />
-        </template>
-      </NoteCard>
-    </div>
+    <NotesList/>
   </div>
 </template>
