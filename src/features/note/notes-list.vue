165d997eb58a40285575f4a86e84acef607377ad
George Susyev <no1621name@gmail.com>
2025-12-13 22:09:40 +0300

    features/note-list: implemented search functionality

--- src/features/note/notes-list.vue
+++ src/features/note/notes-list.vue
@@ -1,12 +1,13 @@
 <script lang="ts" setup>
 import { computed, ref, watchEffect } from 'vue';
 import { TagBadge } from '@/entities/tag';
-import { CreateNoteLink, NoteCard, useGetNotes } from '@/entities/note';
+import { CreateNoteLink, NoteBaseCard, NoteCard, NoteSearchInput, useGetNotes } from '@/entities/note';
 import { useIntersectionObserver } from '@/shared/composables/use-intersection-observer';
 
 const loader = ref<HTMLElement | null>(null);
 
-const { data, fetchNextPage, isLoading, hasNextPage, isFetchingNextPage } = useGetNotes();
+const search = ref('');
+const { data, fetchNextPage, isLoading, hasNextPage, isFetchingNextPage } = useGetNotes(search);
 const notes = computed(() => data.value?.pages.flat() ?? []);
 
 const { isIntersecting } = useIntersectionObserver(loader);
@@ -16,33 +17,52 @@ watchEffect(() => {
     fetchNextPage();
   }
 });
+
+const updateSearch = (value: string) => {
+  search.value = value;
+};
 </script>
 
 <template>
-  <div class="relative grid grid-cols-3 auto-rows-fr gap-3 m-3">
-    <CreateNoteLink/>
-    <NoteCard
-      v-for="note in notes"
-      :key="note.id"
-      :note="note"
-    >
-      <template #tags="{ tags }">
-        <TagBadge
-          v-for="tag in tags"
-          :key="tag.id"
-          :tag="tag"
+  <div class="flex flex-col gap-3 m-3">
+    <NoteSearchInput @update:search="updateSearch"/>
+    <div class="relative grid grid-cols-3 auto-rows-fr gap-3">
+      <CreateNoteLink/>
+
+      <template v-if="notes.length === 0 && search && !isLoading">
+        <NoteBaseCard>
+          <div class="m-auto">
+            <p class="text-lg my-10 text-center break-word">
+              there is no results for "{{ search }}"
+            </p>
+          </div>
+        </NoteBaseCard>
+      </template>
+      <template v-else>
+        <NoteCard
+          v-for="note in notes"
+          :key="note.id"
+          :note="note"
+        >
+          <template #tags="{ tags }">
+            <TagBadge
+              v-for="tag in tags"
+              :key="tag.id"
+              :tag="tag"
+            />
+          </template>
+        </NoteCard>
+      </template>
+
+      <template v-if="isLoading">
+        <div
+          v-for="i in 5"
+          :key="i"
+          class="skeleton w-full h-full min-h-36"
         />
       </template>
-    </NoteCard>
 
-    <template v-if="isLoading">
-      <div
-        v-for="i in 5"
-        :key="i"
-        class="skeleton w-full h-full min-h-36"
-      />
-    </template>
-
-    <div ref="loader" class="absolute bottom-0 w-full h-4"/>
+      <div ref="loader" class="absolute bottom-0 w-full h-4"/>
+    </div>
   </div>
 </template>
