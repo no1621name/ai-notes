af6ed15bbd0549b0cae6f55c5e062225d1a9d61a
George Susyev <no1621name@gmail.com>
2026-01-08 19:34:03 +0300

    added @tiptap/markdown plugin

--- src/shared/lib/tiptap/plugins.ts
+++ src/shared/lib/tiptap/plugins.ts
@@ -2,11 +2,13 @@ import { TaskItem, TaskList } from '@tiptap/extension-list';
 import Typography from '@tiptap/extension-typography';
 import { CharacterCount } from '@tiptap/extensions';
 import StarterKit from '@tiptap/starter-kit';
+import { Markdown } from '@tiptap/markdown';
 
 export const plugins = [
   StarterKit.configure({
     codeBlock: false,
   }),
+  Markdown,
   TaskItem.configure({
     nested: true,
   }),
