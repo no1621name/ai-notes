bd1f707bebdbd8cc5e29bbdd935d85c4ae05d51c
George Susyev <no1621name@gmail.com>
2026-01-08 19:52:48 +0300

    widgets/md-editor: updated usage of editor-bubble-menu component in main-bubble-menu

--- src/widgets/md-editor/main-bubble-menu.vue
+++ src/widgets/md-editor/main-bubble-menu.vue
@@ -1,13 +1,25 @@
 <script lang="ts" setup>
 import { EditorBubbleMenu } from '@/entities/md-editor';
+import { AIHelperToggler, useAiHelper } from '@/features/md-editor/ai-helper';
 import { FormattingDropdown, DEFAULT_FORMATTING_ACTIONS } from '@/features/md-editor/formatting';
 import { QuickActions, QuickActionToggler, useGetQuickActions } from '@/features/md-editor/quick-actions';
+import { useEditor } from '@/entities/md-editor/composables/use-editor';
+
+const { editor } = useEditor();
+const { isActive: isAiHelperActive } = useAiHelper();
 
 const { data: quickActions } = useGetQuickActions(DEFAULT_FORMATTING_ACTIONS);
 </script>
 
 <template>
-  <EditorBubbleMenu>
+  <EditorBubbleMenu
+    v-if="editor"
+    plugin-key-name="main"
+    :should-show="!isAiHelperActive"
+    :floating-options="{
+      placement: 'bottom-start',
+    }"
+  >
     <FormattingDropdown>
       <template #trigger="{toggle}">
         <div class="flex items-center gap-2">
@@ -15,6 +27,7 @@ const { data: quickActions } = useGetQuickActions(DEFAULT_FORMATTING_ACTIONS);
             Styles
           </button>
           <QuickActions v-if="!!quickActions?.length" :quick-actions="quickActions"/>
+          <AIHelperToggler/>
         </div>
       </template>
       <template #option="{ id }">
