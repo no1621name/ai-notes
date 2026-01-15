bf1c35b967c67a34fa03a9ce2899d2eee3eab0a4
George Susyev <no1621name@gmail.com>
2026-01-15 23:15:47 +0300

    shared/ui: added language-select

---
+++ src/shared/ui/language-select.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+import { AVAILABLE_LOCALES, LANGUAGE_NAMES } from '@/app/providers/i18n';
+import { useI18n } from 'vue-i18n';
+import { useRouter } from 'vue-router';
+
+const { push, currentRoute } = useRouter();
+const { locale } = useI18n();
+
+const handleChange = (event: Event) => {
+  const target = event.target as HTMLSelectElement;
+
+  push({
+    name: currentRoute.value.name,
+    params: {
+      ...currentRoute.value.params,
+      locale: target.value,
+    },
+  });
+};
+</script>
+
+<template>
+  <select
+    class="select select-sm"
+    v-model="locale"
+    @change="handleChange"
+  >
+    <option
+      v-for="locale in AVAILABLE_LOCALES"
+      :key="`locale-${locale}`"
+      :value="locale"
+    >
+      {{ LANGUAGE_NAMES[locale] }}
+    </option>
+  </select>
+</template>
