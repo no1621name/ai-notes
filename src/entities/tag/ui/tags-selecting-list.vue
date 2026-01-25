f6c8bc9eb4021ea8909e4f0960e239b1c6181308
George Susyev <no1621name@gmail.com>
2026-01-25 23:54:43 +0300

    entities/tags: added "create" button in TagsSelectingList

--- src/entities/tag/ui/tags-selecting-list.vue
+++ src/entities/tag/ui/tags-selecting-list.vue
@@ -1,7 +1,13 @@
 <script lang="ts" setup>
+import { ref } from 'vue';
+import { useI18n } from 'vue-i18n';
+import VueIcon from '@kalimahapps/vue-icons/VueIcon';
+
 import type { Tag } from '../model/types';
-import TagBadge from './tag-badge.vue';
 import { useGetTags } from '../queries/use-get-tags';
+import { useCreateTag } from '../queries/use-create-tag';
+import TagBadge from './tag-badge.vue';
+import TagForm from './tag-form.vue';
 
 withDefaults(defineProps<{
   selectedTags?: Tag['id'][];
@@ -14,11 +20,14 @@ defineEmits<{
   (e: 'tag-select', id: Tag['id']): void;
 }>();
 
+const { t } = useI18n();
 const { data, isLoading } = useGetTags();
+const { mutate: handleTagCreation } = useCreateTag();
+const showTagForm = ref(false);
 </script>
 
 <template>
-  <div class="flex flex-wrap gap-2">
+  <div class="flex flex-wrap gap-2 items-center">
     <template v-if="isLoading">
       <div
         v-for="i in 5"
@@ -35,6 +44,39 @@ const { data, isLoading } = useGetTags();
         :class="{'cursor-pointer': !isFetching, 'pointer-events-none': isFetching}"
         @click="$emit('tag-select', tag.id)"
       />
+
+      <button
+        v-if="!showTagForm"
+        class="btn"
+        :class="{'btn-circle btn-sm': data?.length, 'btn-xs': !data?.length}"
+        :aria-label="t('createTag')"
+        :title="t('createTag')"
+        @click="showTagForm = true"
+      >
+        <span v-if="!data?.length">
+          {{ t('createTag') }}
+        </span>
+        <VueIcon v-else name="lu:plus"/>
+      </button>
+
+      <TagForm
+        v-if="showTagForm"
+        class="w-60"
+        :autoclose="false"
+        @submit="handleTagCreation"
+        @close="showTagForm = false"
+      />
     </template>
   </div>
 </template>
+
+<i18n>
+{
+  "en": {
+    "createTag": "Create tag"
+  },
+  "ru": {
+    "createTag": "Создать тег"
+  }
+}
+</i18n>
