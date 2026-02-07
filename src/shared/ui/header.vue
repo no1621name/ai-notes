8773abc167f205f2bfbbc5794cfe9a3ea43b3033
George Sysuev <no1621name@gmail.com>
2026-02-07 23:58:27 +0300

    header: added mobile bottom navigation

--- src/shared/ui/header.vue
+++ src/shared/ui/header.vue
@@ -1,10 +1,13 @@
 <script lang="ts" setup>
 import { useI18n } from 'vue-i18n';
 import VueIcon from '@kalimahapps/vue-icons/VueIcon';
-import ThemeController from '@/shared/ui/theme-controller.vue';
-import LanguageSelect from './language-select.vue';
 
-const { t } = useI18n();
+import ThemeController from './theme-controller.vue';
+import LanguageSelect from './language-select.vue';
+import { useIsMobile } from '../composables/use-media-query';
+
+const { t, locale } = useI18n();
+const isMobile = useIsMobile();
 </script>
 
 <template>
@@ -18,16 +21,32 @@ const { t } = useI18n();
     </RouterLink>
 
     <div class="flex items-center gap-2 text-2xl">
-      <RouterLink
-        :to="{ name: 'info', params: { locale: $i18n.locale } }"
-        :title="t('pages.info')"
-        :aria-label="t('pages.info')"
-      >
-        <VueIcon name="lu:info"/>
-      </RouterLink>
+      <div v-if="!isMobile" class="contents">
+        <RouterLink
+          :to="{ name: 'info', params: { locale } }"
+          :title="t('pages.info')"
+          :aria-label="t('pages.info')"
+        >
+          <VueIcon name="lu:info"/>
+        </RouterLink>
+        <slot name="actions"/>
+      </div>
       <ThemeController/>
-      <slot name="actions"/>
       <LanguageSelect/>
     </div>
   </header>
+
+  <nav
+    v-if="isMobile"
+    class="fixed bottom-0 left-0 w-full bg-base-300 z-10 py-4 px-2 flex justify-around items-center rounded-t-2xl text-2xl"
+  >
+    <RouterLink
+      :to="{ name: 'info', params: { locale } }"
+      :title="t('pages.info')"
+      :aria-label="t('pages.info')"
+    >
+      <VueIcon name="lu:info"/>
+    </RouterLink>
+    <slot name="actions"/>
+  </nav>
 </template>
--- src/widgets/layout/header.vue
+++ src/widgets/layout/header.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 import SettingsModal from '@/features/settings/modal.vue';
+import { CreateNoteButton } from '@/entities/note';
 import Header from '@/shared/ui/header.vue';
+import { useIsMobile } from '@/shared/composables/use-media-query';
+
+const isMobile = useIsMobile();
 </script>
 
 <template>
   <Header>
     <template #actions>
+      <CreateNoteButton v-if="isMobile" />
       <SettingsModal/>
     </template>
   </Header>
